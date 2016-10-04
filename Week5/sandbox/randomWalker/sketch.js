3d5d24112374d1e119c96fdf29430e48471dd978
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2016-10-03 20:03:28 -0400

    Velocity is determined by acceleration instead of randomly

--- Week5/sandbox/randomWalker/sketch.js
+++ Week5/sandbox/randomWalker/sketch.js
@@ -7,21 +7,27 @@ function setup() {
 
 function draw() {
   background(0);
-  w.walk();
+  //update the position of the walker
+  w.update();
+  //draw the walker
   w.display();
 }
 
 
 function Walker() {
   this.pos = createVector(width/2, height/2);
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0.1);
   
   this.display = function() {
     fill(255);
     ellipse(this.pos.x, this.pos.y, 50, 50);
   }
   
-  this.walk = function() {
-    this.vel = createVector(random(-5, 5), random(-5, 5));
-    this.pos = this.pos.add(this.vel);
+  this.update = function() {
+    //accelration changes the velocity
+    this.vel.add(this.acc);
+    //velocity changes the position
+    this.pos.add(this.vel);
   }
 }
