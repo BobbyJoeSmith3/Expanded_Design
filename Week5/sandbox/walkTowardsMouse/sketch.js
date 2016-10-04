9cd81ba2ffeffe115e40e5a32915681eb7924b9c
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2016-10-03 21:00:10 -0400

    Create new sketch where acceleration vector is determined by mouse position.

---
+++ Week5/sandbox/walkTowardsMouse/sketch.js
@@ -0,0 +1,38 @@
+var w;
+
+function setup() {
+  createCanvas(640, 400);
+  w = new Walker();
+}
+
+function draw() {
+  background(25);
+  w.update();
+  w.display();
+  
+}
+
+
+function Walker() {
+  this.pos = createVector(width/2, height/2);
+  this.vel = createVector(0, 0);
+  
+  this.update = function() {
+    //calculate acceleration based on mouse position
+    var mouse = createVector(mouseX, mouseY);
+    this.acc = p5.Vector.sub(mouse, this.pos);
+    //set the magnitude of each vector to 1
+    this.acc.normalize();
+    this.acc.div(5);
+    
+    //update velocity and position
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
+  
+  this.display = function() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 30, 30);
+  }
+  
+}
